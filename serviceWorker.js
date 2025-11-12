16eb8454af59e56c3dcd5d8d22dfac94ea53d633
Babuleh78 <jdbdbsbbfdh@gmail.com>
2025-11-12 09:51:47 +0300

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,35 @@
-self.addEventListener('fetch',() => console.log("fetch"));
+const CACHE_NAME = 'attribution-service-v1';
+const urlsToCache = [
+  '/AttributionServiceFrontend/',
+  '/AttributionServiceFrontend/index.html',
+  '/AttributionServiceFrontend/static/js/bundle.js',
+  '/AttributionServiceFrontend/static/css/main.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Для SPA - все запросы возвращаем index.html
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      caches.match('/AttributionServiceFrontend/index.html')
+        .then((response) => {
+          return response || fetch(event.request);
+        })
+    );
+    return;
+  }
+
+  // Для остальных запросов - стандартная логика
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
